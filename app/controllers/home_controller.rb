e4d2b935a775f1e7df4425e3d3b4c9add0dcafac
Alice Clavel <alice.claouis@gmail.com>
2022-07-27 15:21:32 +0200

    feat: add frontend sdk to track page views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    if current_user
-      Analytics.track(event: 'home_visited', user_id: current_user.id, traits: { email: current_user.email })
-    end
+    # if current_user
+    #   Analytics.track(event: 'home_visited', user_id: current_user.id, traits: { email: current_user.email })
+    # end
 
     @fortune = FortuneGem.give_fortune({:max_length => 250})
   end
