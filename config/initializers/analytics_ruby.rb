a21460ded34a325c6f304b0d6fc69000ede4dbf1
Alice Clavel <alice.claouis@gmail.com>
2022-07-27 12:54:56 +0200

    fix: only initialize Analytics if write key is present

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,3 +1,7 @@
 require 'june/analytics'
 
-Analytics = June::Analytics.new({ write_key: Rails.application.credentials[:june_analytics_write_key] })
+june_analytics_key = Rails.application.credentials[:june_analytics_write_key]
+
+if june_analytics_key.present?
+  Analytics = June::Analytics.new({ write_key: june_analytics_key })
+end
