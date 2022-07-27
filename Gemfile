932e397aac5c21b52bb7530d8032585f14e1db20
Alice Clavel <alice.claouis@gmail.com>
2022-07-27 15:16:00 +0200

    feat: add fortune gem

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise'
 gem 'tailwindcss-rails'
 
 gem 'juneso-analytics-ruby', '~> 2.4', '>= 2.4.2'
+gem 'fortune_gem', '~> 0.0.9'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,7 @@ class HomeController < ApplicationController
     if current_user
       Analytics.track(event: 'home_visited', user_id: current_user.id, traits: { email: current_user.email })
     end
+
+    @fortune = FortuneGem.give_fortune({:max_length => 250})
   end
 end
